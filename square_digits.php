37322e1367b7a34e2319bf8d8f3091d124c5a7bf
Adam Turner <hello@adamrichardturner.dev>
2022-11-28 23:55:05 +0000

    Completed kata

--- square_digits.php
+++ square_digits.php
@@ -8,11 +8,17 @@ function square_digits($num): int {
 
     Note: The function accepts an integer and returns an integer
   */
+
+  // Store in $split_num the $num as a string
   $split_num = str_split(strval($num));
+  // Store in squared our squared digits
   $squared = '';
+  // For each digit in $split_num...
   for ($i = 0; $i < count($split_num); $i++) {
+    // Concatenate the digit squared to $squared
     $squared = $squared . $split_num[$i] ** 2;
   }
+  // Return integer representation of $squared
   return intval($squared);
 }
 
