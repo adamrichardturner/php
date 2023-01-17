d8be806cd860c44c51e55419a7653d33a300b11c
Adam Turner <hello@adamrichardturner.dev>
2023-01-17 20:27:25 +0000

    Completed kata

---
+++ simpleDuplicates.php
@@ -0,0 +1,29 @@
+<?php
+
+function solve($arr) {
+    /*
+    Remove the duplicates from a list of integers, keeping 
+    the last ( rightmost ) occurrence of each element.
+
+    Example:
+    For input: [3, 4, 4, 3, 6, 3]
+
+    remove the 3 at index 0
+    remove the 4 at index 1
+    remove the 3 at index 3
+    Expected output: [4, 6, 3]
+    */
+
+    // Store in $clean our non-duplicate values
+    $clean = [];
+    // For each value in $arr, reversed, working right to left
+    // as we want to keep the right-most non-duplicates
+    foreach (array_reverse($arr) as $value) {
+        // If $value is not already in $clean, add it
+        !in_array($value, $clean) ? array_push($clean, $value) : 0;
+    }
+    // Return the reverse of $clean 
+    return array_reverse($clean);
+}
+
+print_r(solve([3, 4, 4, 3, 6, 3])); // [4, 6, 3]
