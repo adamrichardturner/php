a3425687d4dda22b7cbb1c44ac7381dff3a1ccff
Adam Turner <hello@adamrichardturner.dev>
2023-02-10 21:40:09 +0000

    Completed kata

---
+++ boolToWord.php
@@ -0,0 +1,13 @@
+<?php
+
+function boolToWord($bool){
+    /*
+    Complete the method that takes a boolean value 
+    and return a "Yes" string for true, or a "No" 
+    string for false.
+    */
+    return $bool ? "Yes" : "No";
+}
+
+echo boolToWord(true);
+echo boolToWord(false);
